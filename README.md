f5f4e3acc90a3162803b887bff4ce1dec4dd0a8e
Marc Hofmann <m.hofmann@bechtle.com>
2024-03-19 14:10:32 +0100

    copy option

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # exif-rm
+Remove all metadata from images in simple cli program. Call "exif-rm /h" for help.
--- exif-rm/main.cpp
+++ exif-rm/main.cpp
@@ -12,22 +12,37 @@ int main(int argc, char **argv) {
 	namespace fs = std::filesystem;
 
 	std::string path;
+	bool copy = false;
+	std::stringstream sstream;
 	CLI::App app("remove exif data from file or folder.", "exif-rm");
 	app.add_option("-f,--file", path, "the image file or folder to remove data from")->required();
-
+	app.add_flag("-c,--copy", copy, "copy files before overwriting it")->default_val(false);
 	try {
 		CLI11_PARSE(app, argc, argv);
 		Exiv2::ExifData data;
 		
 		if (fs::is_regular_file(path)) {
+			if (copy) {
+				sstream << path << ".orig";
+				fs::copy_file(path, sstream.str());
+			}
 			if (!RemoveFromFile(path)) {
 				std::cout << std::format("Couldn't clear meta data from file \"{}\".\n", path);
+			} else {
+				std::cout << std::format("Successfully cleared meta data from file \"{}\".\n", path);
 			}
 		} else if (fs::is_directory(path)) {
 			for (auto p : fs::directory_iterator(path, fs::directory_options::skip_permission_denied)) {
 				if (fs::is_regular_file(p)) {
+					if (copy) {
+						sstream << p.path().string() << ".orig";
+						fs::copy_file(path, sstream.str());
+						sstream.clear();
+					}
 					if (!RemoveFromFile(p.path().string())) {
 						std::cout << std::format("Couldn't clear meta data from file \"{}\".\n", p.path().string());
+					} else {
+						std::cout << std::format("Successfully cleared meta data from file \"{}\".\n", p.path().string());
 					}
 				}
 			}
